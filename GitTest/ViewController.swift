f95cd6b1c3791e88d3179ed71bd04dfc37a6f49d
appkoder <writeodus@yahoo.com>
2018-03-25 12:11:51 +0100

    first commit

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -10,7 +10,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("first commit")
     }
 
     override func didReceiveMemoryWarning() {
