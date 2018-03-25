39b6ea8250b20b68de130f4679f10ff4efd5dd18
appkoder <writeodus@yahoo.com>
2018-03-25 12:16:02 +0100

    this is a multiline comments used if you do only git commit without the -m option

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("first commit")
+        print("first commit, again modified")
     }
 
     override func didReceiveMemoryWarning() {
